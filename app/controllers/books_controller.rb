c370e4d77860395d40504b80e8f7b13dea7ef57f
ethanortiz25 <ethan@loszitros.com>
2024-01-29 08:11:40 -0600

    flash notices

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = 'Book was successfully created.'
       redirect_to books_path
     else
       # The 'new' action is NOT being called here
@@ -32,6 +33,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
       redirect_to book_path(@book)
     else
       render('edit')
@@ -45,6 +47,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    flash[:notice] = 'Book was successfully destroyed.'
     redirect_to books_path
   end
 
