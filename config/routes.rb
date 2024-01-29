71a0f6aeb524d33fb001b23bb30b2fcbdefcf5cc
root <root@localhost>
2024-01-29 00:15:38 +0000

    project created

---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  root "books#index"
+
+  get 'main/index'
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'tasks/index'
+  # get 'tasks/new'
+  # get 'tasks/edit'
+  # get 'tasks/show'
+  # get 'tasks/delete'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
+  # get 'categories/edit'
+  # get 'categories/delete'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
